5ed2263eba145a041d7659c93e91891a1fbc2a00
Alireza Khodadadi-Jamayran <rezakj@users.noreply.github.com>
2018-04-22 22:08:57 -0400

    Update load10x.R

--- R/load10x.R
+++ R/load10x.R
@@ -2,13 +2,14 @@
 #'
 #' This function takes 10X data files barcodes.tsv, genes.tsv and matrix.mtx and converts them to proper matrix file for scSeqR.
 #' @param dir.10x A directory that includes the 10X barcodes.tsv, genes.tsv and matrix.mtx files.
+#' @param gene.name Should be either geneSymbol or ensembleID.
 #' @return The data frame object.
 #' @examples
 #' \dontrun{
-#' load10x('/hg19')
+#' load10x("/hg19", gene.name = "geneSymbol")
 #' }
 #' @export
-load10x <- function (dir.10x = NULL) {
+load10x <- function (dir.10x = NULL, gene.name = "geneSymbol") {
   if (!dir.exists(dir.10x)) {
     stop("Directory is not provided. Please provide a standard 10x matrix directory
          that includes; barcodes.tsv, genes.tsv and matrix.mtx files")
@@ -27,7 +28,15 @@ load10x <- function (dir.10x = NULL) {
     gene.names.ids <- list.files(dir.10x,full.names=T,"genes.tsv")
     MTX10x <- readMM(MTX10x)
     cell.barcodes <- readLines(cell.barcodes)
+  }
+  if (gene.name == "geneSymbol") {
     gene.names.ids <- as.character(as.matrix(read.table(gene.names.ids,header=F)[2]))
+  }
+  if (gene.name == "ensembleID") {
+    gene.names.ids <- as.character(as.matrix(read.table(gene.names.ids,header=F)[1]))
+  }
+  if (gene.name != "ensembleID" || gene.name != "geneSymbol") {
+    stop("parameter gene.name is missing. gene.name should be ensembleID or geneSymbol")
   }
   colnames(x = MTX10x) <- cell.barcodes
   rownames(x = MTX10x) <- gene.names.ids
