b16a537fde6c839f9637d763d0346ebd41c48776
Khodadadi-Jamayran <khodaa01@heguylab-mbp01.msb.med.nyu.edu>
2018-05-09 14:02:20 -0400

    fixed opt

--- R/opt.clust.num.R
+++ R/opt.clust.num.R
@@ -8,24 +8,33 @@
 #' pot.clust.num(my.obj)
 #' }
 #' @export
-pot.clust.num <- function (x = NULL) {
+pot.clust.num <- function (x = NULL, max.clust = 15, gap.stat.nboot = 100, verbose = TRUE) {
   if ("scSeqR" != class(x)[1]) {
     stop("x should be an object of class scSeqR")
   }
   require(factoextra)
   require(gridExtra)
   df <- x@tsne.data
-  row.names(df) <- df$clls
+  row.names(df) <- df$cells
   df <- df[,-1]
+  df <- df[1:2]
   # Elbow method
-  Elbow = fviz_nbclust(df, kmeans, method = "wss") + geom_vline(xintercept = 4, linetype = 2) + labs(subtitle = "Elbow method")
+  Elbow = fviz_nbclust(df, kmeans, method = "wss", k.max = max.clust) +
+    geom_vline(xintercept = 4, linetype = 2) +
+    labs(subtitle = "Elbow method")
   # Silhouette method
-  Silhouette = fviz_nbclust(df, kmeans, method = "silhouette") + labs(subtitle = "Silhouette method")
+  Silhouette = fviz_nbclust(df, kmeans, method = "silhouette", k.max = max.clust) +
+    labs(subtitle = "Silhouette method")
   # Gap statistic
   # nboot = 50 to keep the function speedy.
   # recommended value: nboot= 500 for your analysis.
   # Use verbose = FALSE to hide computing progression.
   set.seed(123)
-  Gap.statistic = fviz_nbclust(df, kmeans, nstart = 25,  method = "gap_stat", nboot = 50) + labs(subtitle = "Gap statistic method")
+  Gap.statistic = fviz_nbclust(df, kmeans,
+                               nstart = 25,
+                               method = "gap_stat",
+                               nboot = gap.stat.nboot,
+                               k.max = max.clust) +
+    labs(subtitle = "Gap statistic method")
   return(grid.arrange(Elbow, Silhouette,Gap.statistic, nrow = 3))
 }
