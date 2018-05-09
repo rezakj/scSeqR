c7f16a83c09714bba2a7137ec31c0581a719de57
Khodadadi-Jamayran <khodaa01@heguylab-mbp01.msb.med.nyu.edu>
2018-05-09 12:39:58 -0400

    fixed plot1

--- R/stats.plot.R
+++ R/stats.plot.R
@@ -8,22 +8,25 @@
 #' plot.stats(my.obj)
 #' }
 #' @export
-stats.plot <- function (x = NULL) {
+stats.plot <- function (x = NULL, cell.color = "slategray3", box.color = "red", cell.size = 1) {
   if ("scSeqR" != class(x)[1]) {
     stop("x should be an object of class scSeqR")
   }
   DATA <- x@stats
-  mito.percent.plot <- ggplot(DATA,aes(y=mito.percent,x=".", alpha = 0.5)) +
+  mito.percent.plot <- ggplot(DATA,aes(y=mito.percent,x=".")) +
     theme_bw() +
-    geom_jitter(color = "red") +
-    geom_boxplot() + xlab("mito.percent") + ylab("percent of mito genes per cell")
-  nGenes.plot <- ggplot(DATA,aes(y=nGenes,x=".", alpha = 0.5)) +
+    geom_jitter(color = cell.color, size = cell.size, alpha = 0.5) +
+    geom_boxplot(fill = box.color, col = "green", notch = T, outlier.shape = NA, alpha = 0.2) +
+    xlab("mito.percent") + ylab("percent of mito genes per cell")
+  nGenes.plot <- ggplot(DATA,aes(y=nGenes,x=".")) +
     theme_bw() +
-    geom_jitter(color = "red") +
-    geom_boxplot() + xlab("nGenes") + ylab("number of genes per cell")
-  UMIsplot <- ggplot(DATA,aes(y=UMIs,x=".", alpha = 0.5)) +
+    geom_jitter(color = cell.color, size = cell.size, alpha = 0.5) +
+    geom_boxplot(fill = box.color, col = "green", notch = T, outlier.shape = NA, alpha = 0.2) +
+    xlab("nGenes") + ylab("number of genes per cell")
+  UMIsplot <- ggplot(DATA,aes(y=UMIs,x=".")) +
     theme_bw() +
-    geom_jitter(color = "red") +
-    geom_boxplot() + xlab("UMIs") + ylab("number of UMIs per cell")
+    geom_jitter(color = cell.color, size = cell.size, alpha = 0.5) +
+    geom_boxplot(fill = box.color, col = "green", notch = T, outlier.shape = NA, alpha = 0.2) +
+    xlab("UMIs") + ylab("number of UMIs per cell")
   return(grid.arrange(nGenes.plot, UMIsplot,mito.percent.plot, ncol = 3))
 }
