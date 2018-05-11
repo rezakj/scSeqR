86d8de813fbdc924563afecdf787ca9d0b414bf7
Khodadadi-Jamayran <khodaa01@heguylab-mbp01.msb.med.nyu.edu>
2018-05-11 14:35:50 -0400

    objclass

--- R/myclass.R
+++ R/myclass.R
@@ -1,5 +1,4 @@
 setClass("scSeqR", representation (raw.data = "data.frame",
-                                   row.names = "character",
                                    stats = "data.frame",
                                    main.data = "data.frame",
                                    scaled.data = "data.frame",
