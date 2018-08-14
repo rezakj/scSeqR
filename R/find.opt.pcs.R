752456baddfd12248000e7b5f66d0b19dd89483b
Khodadadi-Jamayran <rezakj@users.noreply.github.com>
2018-08-14 11:16:57 -0400

    PCs

--- R/find.opt.pcs.R
+++ R/find.opt.pcs.R
@@ -14,10 +14,12 @@ find.opt.pcs <- function (x = NULL, pcs.in.plot = 50) {
   if ("scSeqR" != class(x)[1]) {
     stop("x should be an object of class scSeqR")
   }
+
+#  DATA <- x@pca.data
   DATA <- x@pca.info$sdev
   OPTpcs <- mean(DATA)*2
   OPTpcs <- (DATA > OPTpcs)
-  OPTpcs <- length(OPTpcs[OPTpcs==TRUE])
+  OPTpcs <- length(OPTpcs[OPTpcs==TRUE]) + 1
   Titel <- paste("Optimal number of PCs (1:", OPTpcs, ")", sep="")
   # fix DATA
   DATA <- as.data.frame(DATA)
@@ -27,14 +29,17 @@ find.opt.pcs <- function (x = NULL, pcs.in.plot = 50) {
   colnames(DATA) <- c("PCs","SDs")
   DATA <- head(DATA,pcs.in.plot)
 ### plot
-  myPLOT <- ggplot(DATA,aes(y=SDs,x=PCs)) +
-    geom_point() + geom_line() +
+  LogSDs <- log2(DATA$SDs)
+  Log2PCs <- log2(DATA$PCs)
+  myPLOT <- ggplot(DATA,aes(y=SDs,x=PCs, col = LogSDs)) +
+    geom_line() + geom_point(size = LogSDs) +
     scale_x_continuous(trans = "log1p") +
     scale_y_continuous(trans = "log1p") +
-    geom_vline(xintercept = OPTpcs,linetype="dotted")+
+    geom_vline(xintercept = OPTpcs,linetype="dotted") +
     theme_bw(base_size = 16)+
     xlab("Principal Components") +
     ylab("PC Standard Deviations") +
+    scale_colour_gradient(low = "gray", high = "red", name="log2 (SDs)") +
     ggtitle(Titel)
 # return
   return(myPLOT)
